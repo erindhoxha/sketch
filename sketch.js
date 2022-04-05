c10e9cab363f4c50645c04305a541659b4ff6c96
Erind <erind@Office-iMac.local>
2022-04-05 15:11:35 +1200

    added squares into sketch

--- sketch.js
+++ sketch.js
@@ -1,13 +1,36 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 600, 600 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+          let w = 60;
+          let h = 60;
+          let gap = 20;
+          let x = 100 + (w + gap) * i;
+          let y = 100 + (h + gap) * j;
+          context.beginPath();
+          context.rect(x, y, w, h);
+          context.stroke(); 
+
+
+
+          context.beginPath();
+          context.rect(x + 8, y + 8, w - 16, h - 16);
+          context.stroke();
+      }
+  }
+
+
+
   };
 };
 
