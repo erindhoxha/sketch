fcdf621a0143da024d34340ae234650d51d9e9bd
Erind <erind@Office-iMac.local>
2022-04-06 16:45:40 +1200

    animating

---
+++ sketch-03.js
@@ -0,0 +1,63 @@
+const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const settings = {
+  dimensions: [ 1080, 1080 ],
+  animate: true
+};
+
+
+const sketch = ({ context, width, height }) => {
+  console.log(context);
+  console.log({});
+  console.log(sketch);
+  console.log(canvasSketch);
+
+  const agents = [];
+  for (let i = 0; i < 40; i++) {
+    let x = random.range(0, width);
+    let y = random.range(0, height);
+    agents.push(new Agent(x, y))
+  }
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+    agents.forEach(agent => {
+      agent.update();
+      agent.draw(context);
+    })
+  };
+};
+
+canvasSketch(sketch, settings);
+
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Agent {
+  constructor(x, y) {
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radius = random.range(4, 12);
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
+  draw(theContext) {
+    theContext.save();
+    theContext.translate(this.pos.x, this.pos.y);
+    theContext.lineWidth = 4;
+    theContext.beginPath();
+    theContext.arc(0, 0, this.radius, 0, Math.PI*2);
+    theContext.fill();
+    theContext.stroke();
+    theContext.restore();
+  }
+}
